f2498c437ecf45c4adaaa9eded5186ce58c7921d
Golovanov <sergey_xg@mail.ru>
2018-09-27 11:21:31 +0300

    update config

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from model.utils import openai_transformer_config
 
 def get_model_config():
     default_config = openai_transformer_config()
-    config = AttrDict({'bpe_vocab_path': './parameters/bpe.vocab',
-                       'bpe_codes_path': './parameters/bpe.code',
-                       'checkpoint_path': './checkpoints/last_checkpoint', 
+    config = AttrDict({'bpe_vocab_path': '../parameters/bpe.vocab',
+                       'bpe_codes_path': '../parameters/bpe.code',
+                       'checkpoint_path': '../checkpoints/last_checkpoint', 
                        'n_layers': default_config.n_layers,
                        'n_pos_embeddings': 1024,
                        'embeddings_size': default_config.embeddings_size,
@@ -15,11 +15,11 @@ def get_model_config():
                        'embed_dropout': default_config.embed_dropout,
                        'attn_dropout': default_config.attn_dropout,
                        'ff_dropout': default_config.ff_dropout,
-                       'max_seq_len': 256,
-                       'beam_size': 1,
+                       'max_seq_len': 320,
+                       'beam_size': 5,
                        'sample': False,
                        'length_penalty': 0.8,
-                       'n_segments': 3})
+                       'n_segments': None})
 
     return config
 
@@ -29,7 +29,7 @@ def get_trainer_config():
                        'batch_size': 256,
                        'batch_split': 64,
                        'lr': 6.25e-5,
-                       'lr_warmup': 5000,
+                       'lr_warmup': 16000,
                        'lm_weight': 0.5,
                        'n_jobs': 4,
                        'label_smoothing': 0.1,
@@ -43,9 +43,7 @@ def get_trainer_config():
                        'interrupt_checkpoint_path': './checkpoints/interrupt_checkpoint',
                        'train_datasets': ['./datasets/ConvAI2/train_self_revised_no_cands.txt',
                                           './datasets/ConvAI2/train_self_original_no_cands.txt',
-                                          './datasets/DailyDialog/train_dailydialog.txt',
-                                          './datasets/Reddit/RC_2012-04.txt',
-                                          './datasets/Reddit/RC_2012-03.txt'],
+                                          './datasets/DailyDialog/train_dailydialog.txt'],
                        'test_datasets': ['./datasets/ConvAI2/valid_self_revised_no_cands.txt',
                                          './datasets/ConvAI2/valid_self_original_no_cands.txt',
                                          './datasets/DailyDialog/valid_dailydialog.txt']})
