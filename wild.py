eabb2a33fa8161093ec64211dcc50ac62d5c56da
Golovanov <sergey_xg@mail.ru>
2018-09-30 21:37:07 +0300

    add wild evaluation

---
+++ wild.py
@@ -0,0 +1,26 @@
+from convai_world import ConvAIWorld
+from parlai.core.params import ParlaiParser
+from agent import TransformerAgent
+
+def main():
+    parser = ParlaiParser(True, True)
+    parser.set_defaults(batchsize=10,
+                        uebok_mod=True)
+
+    ConvAIWorld.add_cmdline_args(parser)
+    TransformerAgent.add_cmdline_args(parser)
+    opt = parser.parse_args()
+
+    agent = TransformerAgent(opt)
+    world = ConvAIWorld(opt, [agent])
+
+    while True:
+        try:
+            world.parley()
+        except Exception as e:
+            print('Exception: {}'.format(e))
+
+
+if __name__ == '__main__':
+    main()
+
