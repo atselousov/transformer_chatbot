3bb636232ac289abb2900584694e39128672bac0
Sergey Golovanov <Sergey_XG@mail.ru>
2018-10-01 01:17:19 +0300

    update parameters for wild

--- wild.py
+++ wild.py
@@ -6,7 +6,22 @@ from agent import TransformerAgent
 def main():
     parser = ParlaiParser(True, True)
     parser.set_defaults(batchsize=10,
-                        wild_mode=True)
+                        sample=True,
+                        wild_mode=True,
+                        replace_repeat=True,
+                        replace_ngram=True,
+                        detokenize=True,
+                        emoji_prob=0.3,
+                        add_questions=0.3,
+                        clean_emoji=True,
+                        check_grammar=True,
+                        correct_generative=True,
+                        split_into_sentences=True,
+                        max_seq_len=256,
+                        beam_size=5,
+                        annealing_topk=None,
+                        annealing=0.6,
+                        length_penalty=0.2)
 
     ConvAIWorld.add_cmdline_args(parser)
     TransformerAgent.add_cmdline_args(parser)
