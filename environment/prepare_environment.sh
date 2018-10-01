8bc31caba3dcf99447a2795d38a56e39c70127c3
atselousov <aleksander.tselousov@yandex.ru>
2018-10-01 08:32:41 +0300

    file path fix

--- environment/prepare_environment.sh
+++ environment/prepare_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir checkpoints
-wget -O file: https://www.dropbox.com/s/cs6zd9yntn6ixea/last_checkpoint?dl=1
+wget -O checkpoints/last_checkpoint https://www.dropbox.com/s/cs6zd9yntn6ixea/last_checkpoint?dl=1
 
 wget -O parameters.zip https://www.dropbox.com/s/n2jbjyq32x6jgr6/parameters.zip?dl=1
 unzip parameters.zip
