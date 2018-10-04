2c664518a05432f2668394dffb792d11c7573ead
atselousov <aleksander.tselousov@yandex.ru>
2018-10-05 00:18:12 +0300

    okey...

--- interactive.py
+++ interactive.py
@@ -5,23 +5,23 @@ if __name__ == '__main__':
     parser = ParlaiParser(add_model_args=True)
 
     parser.set_defaults(model='agent:TransformerAgent',
-                        sample=True,
+                        sample=False,
                         wild_mode=False,
-                        replace_repeat=True,
-                        replace_ngram=True,
-                        detokenize=True,
-                        emoji_prob=0.3,
-                        add_questions=0.4,
-                        clean_emoji=True,
-                        check_grammar=True,
-                        correct_generative=True,
-                        split_into_sentences=True,
+                        replace_repeat=False,
+                        replace_ngram=False,
+                        detokenize=False,
+                        emoji_prob=0,
+                        add_questions=0,
+                        clean_emoji=False,
+                        check_grammar=False,
+                        correct_generative=False,
+                        split_into_sentences=False,
 
                         max_seq_len=256,
                         beam_size=3,
                         annealing_topk=None,
                         annealing=0.6,
-                        length_penalty=0.7)
+                        length_penalty=0.6)
 
     opt = parser.parse_args()
     interactive(opt)
